42a1d4c8f1862479a9d4350a6e157377cebb3103
kiwirevol <sukhopara.nikita@yandex.ru>
2024-03-12 22:53:49 +0300

    second commit

--- src/test/java/ru/netology/service/CashbackHackerServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackerServiceTest.java
@@ -1,45 +1,42 @@
 package ru.netology.service;
 
-
-import static org.junit.Assert.*;
-
 public class CashbackHackerServiceTest {
     CashbackHackerService service = new CashbackHackerService();
 
-    @org.junit.Test
+    @org.testng.annotations.Test
     public void testRemainBoundaryValuesOne() {
-        org.junit.Assert.assertThrows(RuntimeException.class, () -> {
+        org.testng.Assert.assertThrows(RuntimeException.class, () -> {
             service.remain(-1);
         });
     }
 
-    @org.junit.Test
+    @org.testng.annotations.Test
     public void testRemainBoundaryValuesTwo() {
-        org.junit.Assert.assertEquals(1000, service.remain(0));
+        org.testng.Assert.assertEquals(service.remain(0), 1000);
     }
 
-    @org.junit.Test
+    @org.testng.annotations.Test
     public void testRemainBoundaryValuesThree() {
-        org.junit.Assert.assertEquals(999, service.remain(1));
+        org.testng.Assert.assertEquals(service.remain(1), 999);
     }
 
-    @org.junit.Test
+    @org.testng.annotations.Test
     public void testRemainBoundaryValuesFour() {
-        org.junit.Assert.assertEquals(1, service.remain(999));
+        org.testng.Assert.assertEquals(service.remain(999), 1);
     }
 
-    @org.junit.Test
+    @org.testng.annotations.Test
     public void testRemainBoundaryValuesFive() {
-        org.junit.Assert.assertEquals(0, service.remain(1000));
+        org.testng.Assert.assertEquals(service.remain(1000), 0);
     }
 
-    @org.junit.Test
+    @org.testng.annotations.Test
     public void testRemainBoundaryValuesSix() {
-        org.junit.Assert.assertEquals(999, service.remain(1001));
+        org.testng.Assert.assertEquals(service.remain(1001), 999);
     }
 
-    @org.junit.Test
+    @org.testng.annotations.Test
     public void testRemainCriticalPath() {
-        org.junit.Assert.assertEquals(600, service.remain(400));
+        org.testng.Assert.assertEquals(service.remain(400), 600);
     }
 }
